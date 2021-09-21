902c16975eafc4b13181155c30dcb30e0b519c79
KennetQ2903 <kennetquiroz@gmail.com>
2021-09-20 21:29:31 -0600

    6

--- assets/js/main.js
+++ assets/js/main.js
@@ -86,3 +86,164 @@ function dashboard(){
 function title(){
 	window.document.title = "Inicio de sesion"
 }
+
+function actual(actualBtn){
+			let actualId = actualBtn.id;
+			switch(actualId){
+				case "btnDashboard":
+					window.document.title = `Dashboard`;					
+				break;
+
+				case "btnUser":
+					window.document.title = `Usuario`;
+
+				break;
+
+				case "btnGroup":
+					window.document.title = `Cuentas`;
+				break;
+
+				case "btnSuministros":
+					window.document.title = `Suministros`;
+				break;
+
+				case "btnConsumo":
+					window.document.title = `Consumo`;
+				break;
+
+				case "btnNegocio":
+					window.document.title = `Negocios`;
+				break;
+
+				case "btnFacturacion":
+					window.document.title = `Facturacion`;
+				break;
+
+				case "btnBancos":
+					window.document.title = `Cuentas Bancarias`;
+				break;
+
+				case "btnCorreos":
+					window.document.title = `Correos`;
+				break;
+			}
+}
+function logOut(){
+			window.close('Dashboard.html');
+			window.open('index.html');
+}
+
+function posInicial(){
+	$('html, body').animate({
+		scrollTop: $("#menuDashboard").offset().top
+	}, 500);
+}
+
+
+
+// ---------------------------FUNCIONES GRAFICAS CONSUMO---------------------------
+let grafDia = document.getElementById('grafDiaConsumo').getContext("2d");
+var chartDia = new Chart(grafDia, {
+	type: "bar",
+	data: {
+		labels:["6am", "8am", "10am", "12pm", "14pm", "16pm", "18pm"],
+		datasets:[{
+			label: "Gráfica de consumo en el dia (Kw/h)",
+			backgroundColor: "#33FF00",
+			data:[10, 40, 105, 220, 120, 300, 455],
+		}]
+	},
+	options:{
+		responsive: true,
+		maintainAspectRatio: false
+	}
+});
+
+let grafMes = document.getElementById('grafMesConsumo').getContext("2d");
+var chartMes = new Chart(grafMes, {
+	type: "line",
+	data: {
+		labels:["semana 1", "semana 2", "semana 3", "semana 4"],
+		datasets:[{
+			label: "Gráfica de consumo del mes (Kw/h)",
+			borderColor: "rgb(15, 167, 222)",
+			pointBackgroundColor: "rgb(255, 255, 255)", 
+			pointRadius: 6,
+			fill: true,
+			tension: 0.5,
+			data:[4.8333, 11.3333, 10, 15.1666],
+		}]
+	},
+	options:{
+		responsive: true,
+		maintainAspectRatio: false
+	}
+});
+
+let grafAño = document.getElementById('grafAñoConsumo').getContext("2d");
+var chartAño = new Chart(grafAño, {
+	type: "line",
+	data: {
+		labels:["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
+		datasets:[{
+			label: "Gráfica de consumo del año (Kw/h)",
+			borderColor: "#FF031C",
+			pointBackgroundColor: "#FF031C", 
+			pointRadius: 6,
+			fill: true,
+			data:[200, 340, 233.33, 498.9, 367.789, 123.45, 234, 346.7, 100, 98.67, 55.44, 766.55],
+		}]
+	},
+	options:{
+		responsive: true,
+		maintainAspectRatio: false
+	}
+});
+
+
+let grafCl = document.getElementById('grafClientes');
+new Chart(grafCl, {
+	type: "polarArea",
+	data: {
+		labels:["MASESA.S.A", "TOYOTA", "HUAWEI", "SAMSUNG", "UNREAL TECHS"],
+		datasets:[{
+			label: "Gráfica de clientes",
+			backgroundColor: [
+		      'rgb(255, 99, 132)',
+		      'rgb(75, 192, 192)',
+		      'rgb(255, 205, 86)',
+		      'rgb(201, 203, 207)',
+		      'rgb(54, 162, 235)'],
+			data:[30, 3, 8, 12, 34],
+		}]
+	},
+	options:{
+		responsive: true,
+		maintainAspectRatio: false
+	}
+});
+
+/*==================================== MENU CONSUMO ===========================*/
+function posMes(){
+			document.getElementById('mesTab').style.top = -100 + '%';
+			document.getElementById('añoTab').style.top = 200 + '%';
+
+			document.getElementById('btnDia').style.background = "rgba(0, 0, 0, 0)";
+			document.getElementById('btnMes').style.background = "#333692";
+			document.getElementById('btnAño').style.background = "rgba(0, 0, 0, 0)";
+		}
+		function posDia(){
+			document.getElementById('mesTab').style.top = 100 + '%';
+			document.getElementById('añoTab').style.top = 200 + '%';	
+
+			document.getElementById('btnDia').style.background = "#333692";
+			document.getElementById('btnMes').style.background = "rgba(0, 0, 0, 0)";
+			document.getElementById('btnAño').style.background = "rgba(0, 0, 0, 0)";	
+		}
+		function posAño(){
+			document.getElementById('añoTab').style.top = -200 + '%';
+
+			document.getElementById('btnDia').style.background = "rgba(0, 0, 0, 0)";
+			document.getElementById('btnMes').style.background = "rgba(0, 0, 0, 0)";
+			document.getElementById('btnAño').style.background = "#333692";	
+		}
